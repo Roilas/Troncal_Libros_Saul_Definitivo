dc126c3d161b9a4fe7627cef661bd1ebf90f6d95
JOBFER <gbringashotmail@hotmail.com>
2018-05-30 22:22:14 +0200

    Inicio

---
+++ src/main/java/com/journaldev/spring/service/EmployeeService.java
@@ -0,0 +1,65 @@
+package com.journaldev.spring.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import com.journaldev.jpa.data.Employee;
+
+@Component
+public class EmployeeService {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+
+	@Autowired
+    private PlatformTransactionManager transactionManager;
+	
+	@Transactional
+	public void register(Employee emp) {
+		// Save employee
+		this.em.persist(emp);
+		
+		
+//		//Forma 1: Crear una transaccion propia
+//		TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+//		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+//	        @Override
+//	        protected void doInTransactionWithoutResult(TransactionStatus status) {
+//	            em.createNativeQuery("TRUNCATE TABLE Employee").executeUpdate();
+//	        }
+//	    });
+
+		
+//		//Forma 2: Crear una transaccion propia	
+//		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+//		def.setName("miTransaccion");
+//		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+//		TransactionStatus status = transactionManager.getTransaction(def);
+//		try {
+//			em.createNativeQuery("TRUNCATE TABLE Employee").executeUpdate();
+//		}
+//		catch (Exception ex) {
+//			transactionManager.rollback(status);
+//		}
+//		transactionManager.commit(status);
+	}
+
+}
